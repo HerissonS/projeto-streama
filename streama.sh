1c9dc187cbf84f8b2c6364c612f0205edc2c6e05
Herisson Silva <antonioherisson@gmail.com>
2023-08-08 19:30:20 -0300

    Atualizacao implementacao

--- streama.sh
+++ streama.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 sudo apt install docker-compose
-sudo mkdir --parents /mnt/efs/ctfoco/streama/docker
-sudo mkdir --parents /mnt/efs/ctfoco/streama/ldap
-sudo mkdir --parents /mnt/efs/ctfoco/streama/signup
+sudo mkdir --parents /streama/docker
+sudo mkdir --parents /streama/ldap
+sudo mkdir --parents /streama/signup
 sudo chmod 777 -R /streama
-mv ./LDAP/* /mnt/efs/ctfoco/streama/ldap/
-mv ./SIGNUP/* /mnt/efs/ctfoco/streama/signup/
-mv ./STREAMA/* /mnt/efs/ctfoco/streama/docker/
-cd /mnt/efs/ctfoco/streama/ldap
+mv ./LDAP/* /streama/ldap/
+mv ./SIGNUP/* /streama/signup/
+mv ./STREAMA/* /streama/docker/
+cd /streama/ldap
 sudo docker-compose up -d
-cd /mnt/efs/ctfoco/streama/signup
+cd /streama/signup
 sudo docker-compose up -d
-cd /mnt/efs/ctfoco/streama/docker
+cd /streama/docker
 wget https://github.com/streamaserver/streama/releases/download/v1.10.4/streama-1.10.4.jar
 sudo docker-compose up -d
 sudo chown ubuntu:ubuntu -R /streama
